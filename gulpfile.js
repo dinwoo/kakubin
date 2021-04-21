d86631117afa84fcd823027550fd51eed3325ffe
din <danielforboy@gmail.com>
2021-04-22 00:26:18 +0800

    feat: leading n banner

--- gulpfile.js
+++ gulpfile.js
@@ -1,191 +1,136 @@
-var gulp = require("gulp");
-var $ = require("gulp-load-plugins")();
-var autoprefixer = require("autoprefixer");
-var mainBowerFiles = require("main-bower-files");
-var browserSync = require("browser-sync").create();
-var minimist = require("minimist");
+var gulp  =require('gulp');
+var $ = require('gulp-load-plugins')();
+var autoprefixer = require('autoprefixer');
+var mainBowerFiles = require('main-bower-files');
+var browserSync = require('browser-sync').create();
+var minimist = require('minimist');
+
 
 var envOptions = {
-  sting: "env",
-  default: { env: "develop" },
-};
+  sting: 'env',
+  default: {env: 'develop'}
+}
 
-var options = minimist(process.argv.slice(2), envOptions);
+var options = minimist(process.argv.slice(2), envOptions)
 
-gulp.task("clean", function () {
-  return gulp
-    .src(
-      $.if(
-        options.env === "production",
-        [("./.tmp", "./dist")],
-        [("./.tmp", "./public")]
-      ),
-      {
-        read: false,
-        allowEmpty: true,
-      }
-    )
+
+gulp.task('copyAssets',function () {
+  return gulp.src('./source/assets/**')
+  .pipe(gulp.dest('./public/'))
+})
+
+gulp.task('clean', function () {
+  return gulp.src(['./.tmp','./public'], {
+    read: false,
+    allowEmpty:true
+  })
     .pipe($.clean());
 });
 
-gulp.task("pug", function () {
-  return gulp
-    .src("./source/views/**/*.pug")
+gulp.task('pug',function () {
+  // return gulp.src(['./source/**/*.pug','./source/views/**/*.pug'])
+  return gulp.src('./source/views/**/*.pug')
     .pipe($.plumber())
-    .pipe(
-      $.pug({
-        pretty: true,
-      })
-    )
-    .pipe(
-      $.if(
-        options.env === "production",
-        gulp.dest("./dist/"),
-        gulp.dest("./public/")
-      )
-    )
-    .pipe(browserSync.stream());
-});
-
-gulp.task("sass", function () {
-  return (
-    gulp
-      .src("./source/sass/**/*.sass")
-      .pipe($.plumber())
-      .pipe($.sourcemaps.init())
-      .pipe($.sass().on("error", $.sass.logError))
-      // 編譯完成
-      .pipe($.postcss([autoprefixer()]))
-      .pipe($.if(options.env === "production", $.cleanCss()))
-      .pipe($.sourcemaps.write("."))
-      .pipe(
-        $.if(
-          options.env === "production",
-          gulp.dest("./dist/css"),
-          gulp.dest("./public/css")
-        )
-      )
-      .pipe(browserSync.stream())
-  );
-});
-
-gulp.task("babel", () =>
-  gulp
-    .src("./source/js/**/*.js")
-    .pipe($.sourcemaps.init())
-    .pipe(
-      $.babel({
-        presets: ["@babel/env"],
-      })
-    )
-    .pipe($.concat("all.js"))
-    .pipe(
-      $.if(
-        options.env === "production",
-        $.uglify({
-          compress: {
-            drop_console: true,
-          },
-        })
-      )
-    )
-    .pipe($.sourcemaps.write("."))
-    .pipe(
-      $.if(
-        options.env === "production",
-        gulp.dest("./dist/js"),
-        gulp.dest("./public/js")
-      )
-    )
+    .pipe($.pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./public/'))
     .pipe(browserSync.stream())
+})
+
+
+gulp.task('sass', function () {
+  return gulp.src('./source/sass/**/*.sass')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass().on('error', $.sass.logError))
+    // 編譯完成
+    .pipe($.postcss([autoprefixer()]))
+    .pipe($.if(options.env==='production',$.cleanCss()))
+    .pipe($.sourcemaps.write('.'))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream())
+});
+
+gulp.task('babel', () =>
+    gulp.src('./source/js/**/*.js')
+        .pipe($.sourcemaps.init())
+        .pipe($.babel({
+            presets: ['@babel/env']
+        }))
+        // .pipe($.concat('all.js'))
+        .pipe($.if(options.env==='production',$.uglify({
+          compress: {
+            drop_console: false
+          }
+        })))
+        .pipe($.sourcemaps.write('.'))
+        .pipe(gulp.dest('./public/js'))
+        .pipe(browserSync.stream())
 );
 
-gulp.task("bower", function () {
-  return gulp
-    .src(
-      mainBowerFiles({
-        overrides: {
-          vue: {
-            // 套件名稱
-            main: "dist/vue.js", // 取用的資料夾路徑
-          },
-        },
-      })
-    )
-    .pipe(gulp.dest("./.tmp/vendors"));
-  cb(err);
+gulp.task('vendorJs',function () {
+  return gulp.src('./.tmp/vendors/**/*.js')
+    .pipe($.concat('vendors.js'))
+    .pipe($.if(options.env==='production',$.uglify()))
+    .pipe(gulp.dest('./public/js'))
 });
 
-gulp.task("vendorJs", function () {
-  return gulp
-    .src("./.tmp/vendors/**/*.js")
-    .pipe($.concat("vendors.js"))
-    .pipe($.if(options.env === "production", $.uglify()))
-    .pipe(
-      $.if(
-        options.env === "production",
-        gulp.dest("./dist/js"),
-        gulp.dest("./public/js")
-      )
-    );
+gulp.task('image-min',function () {
+  return gulp.src('./source/assets/images/*')
+  .pipe($.if(options.env==='production',$.imagemin()))
+  .pipe(gulp.dest('./public/images'))
+})
+
+gulp.task('browser-sync', function() {
+  return browserSync.init({
+    server: {
+      baseDir: "./public",
+      // reloadDebounce: 2000 //重新整理的間隔必須超過 2 秒
+    }
+  });
 });
 
-gulp.task("image-min", function () {
-  return gulp
-    .src("./source/images/*")
-    .pipe($.if(options.env === "production", $.imagemin()))
-    .pipe(
-      $.if(
-        options.env === "production",
-        gulp.dest("./dist/images"),
-        gulp.dest("./public/images")
-      )
-    );
+gulp.task('deploy', function() {
+  return gulp.src('./public/**/*')
+    .pipe($.ghPages());
 });
 
-gulp.task("deploy", function () {
-  return gulp.src("./dist/**/*").pipe($.ghPages());
+gulp.task('watch', function () {
+  gulp.watch('./source/sass/**/*.sass', gulp.series('sass'));
+  gulp.watch('./source/js/**/*.js', gulp.series('babel'));
+  gulp.watch('./source/**/*.pug', gulp.series('pug'));
+  // gulp.watch('./source/**/*.jade', gulp.series('jade'));
 });
 
-gulp.task("clean-publish", function () {
-  return gulp
-    .src(["./.publish"], {
-      read: false,
-      allowEmpty: true,
-    })
-    .pipe($.clean());
-});
-
-gulp.task(
-  "build",
+gulp.task('build',
   gulp.series(
-    "clean",
-    "bower",
-    "vendorJs",
-    gulp.parallel("pug", "sass", "babel", "image-min")
-  )
-);
+    'clean',
+    'vendorJs',
+    gulp.parallel('copyAssets','pug', 'sass', 'babel','image-min')));
+// gulp.task('build', gulp.series(gulp.series('clean','jade','sass','babel','bower','vendorJs')))
 
-gulp.task(
-  "default",
+// gulp.task('default', gulp.series('pug','sass','babel','bower','vendorJs','image-min',gulp.parallel('browser-sync','watch')))
+
+gulp.task('default',
   gulp.series(
-    "clean",
-    "bower",
-    "vendorJs",
-    gulp.parallel("pug", "sass", "babel", "image-min"),
-    function (done) {
+    'clean',
+    'vendorJs',
+    gulp.parallel('copyAssets','pug', 'sass', 'babel','image-min'),
+    function(done) {    
       browserSync.init({
         server: {
           baseDir: "./public",
-          reloadDebounce: 2000, //重新整理的間隔必須超過 2 秒
-        },
+          reloadDebounce: 2000 //重新整理的間隔必須超過 2 秒
+        }
       });
-
-      gulp.watch("./source/sass/**/*.sass", gulp.series("sass"));
-      gulp.watch("./source/js/**/*.js", gulp.series("babel"));
-      gulp.watch("./source/**/*.pug", gulp.series("pug"));
+      
+      gulp.watch('./source/sass/**/*.sass', gulp.series('sass'));
+      gulp.watch('./source/js/**/*.js', gulp.series('babel'));
+      gulp.watch('./source/**/*.pug', gulp.series('pug'));
 
       done();
     }
+    
   )
 );
