49c38b1d21733d762ae629486c0bcf714defc4f9
din <danielforboy@gmail.com>
2021-04-24 00:31:44 +0800

    Update 2021-04-23T16:31:41.310Z

--- js/index.js
+++ js/index.js
@@ -1,2 +1,2 @@
-"use strict";var fun=function(){console.log("!!!!")};fun();
+"use strict";!function(o){o(document).ready(function(){o("#video-carousel").owlCarousel({loop:!0,margin:40,responsiveClass:!0,autoplay:!0,autoplayTimeout:5e3,autoplayHoverPause:!0,items:1.2,nav:!1,center:!0,dots:!0})})}($);
 //# sourceMappingURL=index.js.map
