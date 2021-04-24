e513ea1491b8976017adc7f18e19547460f9c042
Daniel Wu <daniel.wu@language-center.com.tw>
2021-04-24 12:09:18 +0800

    feat: setmeal輪播

--- source/js/index.js
+++ source/js/index.js
@@ -288,6 +288,19 @@
       dots: true,
     });
 
+    let setmealOwl = $("#setmeal-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      responsiveClass: true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: true,
+      items: 1,
+      nav: false,
+      center: true,
+      dots: true,
+    });
+
     $(".menu-item:nth-child(1)").on("click", function () {
       closeMenu();
       $("html,body").animate(
