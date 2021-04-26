18bb862223d60897d5e21f1d850a5366e4c18b44
din <danielforboy@gmail.com>
2021-04-27 00:02:35 +0800

    feat: 補資料跟動態

--- source/js/index.js
+++ source/js/index.js
@@ -1,51 +1,67 @@
 (function ($) {
   function setAnimate() {
-    TweenMax.set("section.introduce h1", {
+    TweenMax.set("section.banner .cow", {
       opacity: 0,
-      y: 50,
+      x: 50,
+      y: 20,
     });
 
     TweenMax.set(
-      "section.introduce .part:nth-of-type(1) .info,section.introduce .part:nth-of-type(2) .pic,section.introduce .part:nth-of-type(3) .info,section.introduce .part:nth-of-type(4) .pic,section.introduce .part:nth-of-type(5) .info",
+      "section.banner figure.pic-1,section.banner figure.pic-2",
       {
         opacity: 0,
-        x: 100,
+        x: 80,
+        y: -15
       }
     );
 
     TweenMax.set(
-      "section.introduce .part:nth-of-type(1) .pic,section.introduce .part:nth-of-type(2) .info,section.introduce .part:nth-of-type(3) .pic,section.introduce .part:nth-of-type(4) .info,section.introduce .part:nth-of-type(5) .pic",
+      "section.banner figure.pic-3",
       {
         opacity: 0,
-        x: -100,
+        x: -50,
       }
     );
 
-    TweenMax.set("section.certification h1", {
-      opacity: 0,
-      y: 50,
-    });
+    TweenMax.set(
+      "section.banner figure.pic-4",
+      {
+        opacity: 0,
+        x: 50,
+      }
+    );
 
-    TweenMax.set("section.certification .box .pic", {
-      opacity: 0,
-      y: 50,
-    });
+    TweenMax.set(
+      "section.banner .slogan-txt,section.video,section.setmeal,section.teaching,section.cooperation",
+      {
+        opacity: 0,
+        y: 50,
+      }
+    );
+
+    // TweenMax.set("section.certification h1", {
+    //   opacity: 0,
+    //   y: 50,
+    // });
+
+    // TweenMax.set("section.certification .box .pic", {
+    //   opacity: 0,
+    //   y: 50,
+    // });
   }
   function doAnimate() {
     var controller = new ScrollMagic.Controller();
     // ------------------------------------------------------------------------------------------------------
 
-    var itemTween1 = new TimelineMax().add(
-      TweenMax.to("section.introduce h1", 1, {
-        opacity: 1,
-        x: 0,
-        y: 0,
-        ease: Power1.easeOut,
-      })
-    );
+    var itemTween1 = new TimelineMax()
+      .add(TweenMax.to('section.banner .cow', 0.5, { opacity: 1, x: 0, y: 0, ease: Power1.easeOut }))
+      .add(TweenMax.to('section.banner figure.pic-1', 0.5, { opacity: 1, x: 0, y: 0, ease: Power1.easeOut }))
+      .add(TweenMax.to('section.banner figure.pic-2', 0.5, { opacity: 1, x: 0, y: 0,delay: -.2, ease: Power1.easeOut }))
+      .add(TweenMax.to('section.banner figure.pic-3,section.banner figure.pic-4', 0.5, { opacity: 1, x: 0, y: 0, ease: Power1.easeOut }))
+      .add(TweenMax.to('section.banner .slogan-txt', 0.5, { opacity: 1, x: 0, y: 0,delay: -.5, ease: Power1.easeOut }))
 
     var itemScene1 = new ScrollMagic.Scene({
-      triggerElement: "section.introduce h1",
+      triggerElement: "section.banner",
       offset: -100, //指標位移
       triggerHook: 0.5,
       reverse: false, //動畫重複 default:true
@@ -56,28 +72,12 @@
     // ------------------------------------------------------------------------------------------------------
 
     var itemTween2 = new TimelineMax()
-      .add(
-        TweenMax.to("section.introduce .part:nth-of-type(1) .info", 0.5, {
-          opacity: 1,
-          x: 0,
-          y: 0,
-          ease: Power1.easeOut,
-        })
-      )
-      .add(
-        TweenMax.to("section.introduce .part:nth-of-type(1) .pic", 0.5, {
-          opacity: 1,
-          x: 0,
-          y: 0,
-          delay: -0.5,
-          ease: Power1.easeOut,
-        })
-      );
+      .add(TweenMax.to('section.video', 0.5, { opacity: 1, x: 0, y: 0, ease: Power1.easeOut }))
 
     var itemScene2 = new ScrollMagic.Scene({
-      triggerElement: "section.introduce .part:nth-of-type(1)",
-      offset: 0, //指標位移
-      triggerHook: 0.6,
+      triggerElement: "section.video",
+      offset: -100, //指標位移
+      triggerHook: 0.5,
       reverse: false, //動畫重複 default:true
     })
       .setTween(itemTween2)
@@ -86,28 +86,12 @@
     // ------------------------------------------------------------------------------------------------------
 
     var itemTween3 = new TimelineMax()
-      .add(
-        TweenMax.to("section.introduce .part:nth-of-type(2) .info", 0.5, {
-          opacity: 1,
-          x: 0,
-          y: 0,
-          ease: Power1.easeOut,
-        })
-      )
-      .add(
-        TweenMax.to("section.introduce .part:nth-of-type(2) .pic", 0.5, {
-          opacity: 1,
-          x: 0,
-          y: 0,
-          delay: -0.5,
-          ease: Power1.easeOut,
-        })
-      );
+      .add(TweenMax.to('section.setmeal', 0.5, { opacity: 1, x: 0, y: 0, ease: Power1.easeOut }))
 
     var itemScene3 = new ScrollMagic.Scene({
-      triggerElement: "section.introduce .part:nth-of-type(2)",
-      offset: 0, //指標位移
-      triggerHook: 0.6,
+      triggerElement: "section.setmeal",
+      offset: -100, //指標位移
+      triggerHook: 0.5,
       reverse: false, //動畫重複 default:true
     })
       .setTween(itemTween3)
@@ -116,28 +100,12 @@
     // ------------------------------------------------------------------------------------------------------
 
     var itemTween4 = new TimelineMax()
-      .add(
-        TweenMax.to("section.introduce .part:nth-of-type(3) .info", 0.5, {
-          opacity: 1,
-          x: 0,
-          y: 0,
-          ease: Power1.easeOut,
-        })
-      )
-      .add(
-        TweenMax.to("section.introduce .part:nth-of-type(3) .pic", 0.5, {
-          opacity: 1,
-          x: 0,
-          y: 0,
-          delay: -0.5,
-          ease: Power1.easeOut,
-        })
-      );
+      .add(TweenMax.to('section.teaching', 0.5, { opacity: 1, x: 0, y: 0, ease: Power1.easeOut }))
 
     var itemScene4 = new ScrollMagic.Scene({
-      triggerElement: "section.introduce .part:nth-of-type(3)",
-      offset: 0, //指標位移
-      triggerHook: 0.6,
+      triggerElement: "section.teaching",
+      offset: -100, //指標位移
+      triggerHook: 0.5,
       reverse: false, //動畫重複 default:true
     })
       .setTween(itemTween4)
@@ -146,104 +114,19 @@
     // ------------------------------------------------------------------------------------------------------
 
     var itemTween5 = new TimelineMax()
-      .add(
-        TweenMax.to("section.introduce .part:nth-of-type(4) .info", 0.5, {
-          opacity: 1,
-          x: 0,
-          y: 0,
-          ease: Power1.easeOut,
-        })
-      )
-      .add(
-        TweenMax.to("section.introduce .part:nth-of-type(4) .pic", 0.5, {
-          opacity: 1,
-          x: 0,
-          y: 0,
-          delay: -0.5,
-          ease: Power1.easeOut,
-        })
-      );
+      .add(TweenMax.to('section.cooperation', 0.5, { opacity: 1, x: 0, y: 0, ease: Power1.easeOut }))
 
     var itemScene5 = new ScrollMagic.Scene({
-      triggerElement: "section.introduce .part:nth-of-type(4)",
-      offset: 0, //指標位移
-      triggerHook: 0.6,
+      triggerElement: "section.cooperation",
+      offset: -100, //指標位移
+      triggerHook: 0.5,
       reverse: false, //動畫重複 default:true
     })
       .setTween(itemTween5)
       // .addIndicators({ name: "item05" }) // 指標顯示
       .addTo(controller);
-    // ------------------------------------------------------------------------------------------------------
-
-    var itemTween6 = new TimelineMax()
-      .add(
-        TweenMax.to("section.introduce .part:nth-of-type(5) .info", 0.5, {
-          opacity: 1,
-          x: 0,
-          y: 0,
-          ease: Power1.easeOut,
-        })
-      )
-      .add(
-        TweenMax.to("section.introduce .part:nth-of-type(5) .pic", 0.5, {
-          opacity: 1,
-          x: 0,
-          y: 0,
-          delay: -0.5,
-          ease: Power1.easeOut,
-        })
-      );
-
-    var itemScene6 = new ScrollMagic.Scene({
-      triggerElement: "section.introduce .part:nth-of-type(5)",
-      offset: 0, //指標位移
-      triggerHook: 0.6,
-      reverse: false, //動畫重複 default:true
-    })
-      .setTween(itemTween6)
-      // .addIndicators({ name: "item06" }) // 指標顯示
-      .addTo(controller);
-    // ------------------------------------------------------------------------------------------------------
-
-    var itemTween7 = new TimelineMax().add(
-      TweenMax.to("section.certification h1", 1, {
-        opacity: 1,
-        x: 0,
-        y: 0,
-        ease: Power1.easeOut,
-      })
-    );
-
-    var itemScene7 = new ScrollMagic.Scene({
-      triggerElement: "section.certification h1",
-      offset: -100, //指標位移
-      triggerHook: 0.5,
-      reverse: false, //動畫重複 default:true
-    })
-      .setTween(itemTween7)
-      // .addIndicators({ name: "item07" }) // 指標顯示
-      .addTo(controller);
-    // ------------------------------------------------------------------------------------------------------
-
-    var itemTween8 = new TimelineMax().add(
-      TweenMax.staggerTo(
-        "section.certification .box .pic",
-        0.5,
-        { opacity: 1, y: 0, ease: Power1.easeOut },
-        0.2
-      )
-    );
-
-    var itemScene8 = new ScrollMagic.Scene({
-      triggerElement: "section.certification .box",
-      offset: 0, //指標位移
-      triggerHook: 0.5,
-      reverse: false, //動畫重複 default:true
-    })
-      .setTween(itemTween8)
-      // .addIndicators({ name: "item08" }) // 指標顯示
-      .addTo(controller);
   }
+
   function closeMenu() {
     $(".menu").removeClass("active");
     $(".ham").removeClass("rotate");
@@ -254,9 +137,12 @@
 
   $(document).ready(function () {
     let isOpen = false;
+    setAnimate();
+    // doAnimate();
 
     $(".yes").on("click", function () {
       $("section.leading-page").hide();
+      doAnimate();
     });
     $(".no").on("click", function () {
       window.history.go(-1);
@@ -337,7 +223,5 @@
       );
     });
 
-    // setAnimate();
-    // doAnimate();
   });
 })($);
